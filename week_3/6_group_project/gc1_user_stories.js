b2c35e82e60099087c6e12293b0111542dfec293
Fahia Moh <fahia_mohamed@hotmail.com>
2014-05-08 12:09:28 -0400

    updated user story

--- week_3/6_group_project/gc1_user_stories.js
+++ week_3/6_group_project/gc1_user_stories.js
@@ -1,10 +1,10 @@
 I want to..
-	1. create a variable called "sum" that will represent the sum of the numbers within the given range*. 
-	2. Then, have that variable return the sum of all odd numbers within the oddLengthArray. 
-	3. Then return the sum of all even numbers within the evenLengthArray for the third test. 
+	1. create a variable called "sum" that will represent the sum of the numbers within the given range. 
+	2. Then, have that variable return the sum of all the numbers within the oddLengthArray. 
+	3. Then return the sum of all the numbers within the evenLengthArray for the third test. 
 	4. For the fourth test, I want to create a variable called "mean" that will represent the average of the given range of numbers.  
-	5. This mean variable will return the average of all the odd numbers within the oddLengthArray.  
-	6. This will also be done with the average of all the even numbers within the evenLengthArray
+	5. This mean variable will return the average of all the the numbers within the oddLengthArray.  
+	6. This will also be done with the average of all the numbers within the evenLengthArray
 	7. create a variable called median that will represent the median of the given ranges.
-	8. This variable will return the median value of all numbers within the oddLengthArray that are odd.
-	9. Then, the variable will return the median of all even numbers within the evenLengthArray.
+	8. This variable will return the median value of all numbers within the oddLengthArray.
+	9. Then, the variable will return the median of all numbers within the evenLengthArray.
