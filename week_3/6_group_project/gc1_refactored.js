c5e65b885755a8090deb7ec616e741f8349b1190
Fahia Moh <fahia_mohamed@hotmail.com>
2014-05-11 23:32:10 -0400

    updated reflection

---
+++ week_3/6_group_project/gc1_refactored.js
@@ -0,0 +1,63 @@
+oddLengthArray  = [1, 2, 3, 4, 5, 5, 7]
+evenLengthArray = [4, 4, 5, 5, 6, 6, 6, 7]
+
+//USER STORY
+//As a user, I want to find the total sum of a variety of numbers (i.e. test scores). I want sum to add all the individual //test scores and give me the total sum.
+
+// Create variable "sum" that represents sum of numbers within a given range (a,b).
+//Refactored sum
+var sum = function(range) {
+  var total = 0
+  for  (var i=0; i<range.length; i++) {
+    total += range[i]
+  }
+    return total;
+  
+}
+
+
+//Refactored Tests
+sum(oddLengthArray);
+
+//Refactored Tests
+sum(evenLengthArray);
+
+//User Story
+//As a user, I want to find the average of a set of numbers. So, if I had 3 test scores (100, 25, 61), mean would return the number 62, because 62 is the average score of the three tests.
+
+// set variable mean = sum(range)/(b-a). 
+//Refactored same as original 
+var mean= function(range) {
+  return sum(range)/range.length
+}; 
+
+//Refactored Test
+mean(oddLengthArray);
+
+
+//Refactored Test
+mean(evenLengthArray);
+
+
+//User Story
+//As a user, I want median to find the middle number of a set of numbers. So if I had numbers 1, 2, 3, 4, and 5, median //would return the number 3. However, if the list comprised of 1, 2, 3, 4, 5, and 6, median would return 3.5 because //median would take the two middle numbers, add them together and divide the sum by 2. Also, median will need to make sure //that the set of numbers are sorted from least to greatest or vice versa.
+
+// Set variable median(range). Sort range and determine median depending on whether range length is even or odd.
+//Refactored code
+var median= function(range){
+  var sortedRange=range.sort();
+  var halfLength=Math.floor(range.length/2);
+   if (range.length % 2 === 0) {
+      return ((sortedRange[halfLength-1]+sortedRange[halfLength])/2);
+    }
+  else {
+    return sortedRange[halfLength];
+  }
+}
+
+//Refactored Test
+median(oddLengthArray);
+
+
+//Refactored Test
+median(evenLengthArray);
--- week_3/6_group_project/gc1_user_stories.js
+++ week_3/6_group_project/gc1_user_stories.js
@@ -8,3 +8,35 @@ I want to..
 	7. create a variable called median that will represent the median of the given ranges.
 	8. This variable will return the median value of all numbers within the oddLengthArray.
 	9. Then, the variable will return the median of all numbers within the evenLengthArray.
+
+// Reflection
+// What was this experience like?
+// I've never heard of a user story before so researching it and seeing its benefits to structuring the process for going about a project.
+// It was interesting to see everyone take a portion of a project and take the finished product and incorporate all our work into one.
+
+// Was your group successful in passing the tests?
+// My group did well in passing every test and we all did well on our parts of the challenge
+
+// What did you learn about writing user stories, pseudocoding, writing code, refactoring, etc.?
+// I learned that there was a process in working towards a solution/project. This involves many steps and I felt this was a very
+// organized, clean method of going about a problem.  It ensured we thought everything through and walked through it step by step.
+
+// What parts of your strategy worked? What problems did you face?
+// In writing the user story, I didn't find a problem.  I was focused on providing a clear, set of instructions that would be
+// easily implemented into pseudocode.
+
+// What questions did you have while coding? What resources did you find to help you answer them?
+
+// What concepts are you having trouble with, or did you just figure something out? If so, what?
+
+// Did you learn any new skills or tricks?
+// This entire challenge showed various new skills about going through a solution and I think these are not tedious or redundant because
+// it is easily scalable to more complex projects that require clear, defined set of steps and ways to go about it. 
+
+// How confident are you with each of the Learning Competencies?
+// I felt I got a great grasp on the learning competencies and how each step is necessary in the overall solution
+
+// Which parts of the challenge did you enjoy?
+// Which parts of the challenge did you find tedious?
+// Neither of these really describe how this was for me.  It wasn't either enjoyable or tedious. 
+// I did however, gain a new perspective on problem solving and how developers go through tackling a problem.  
